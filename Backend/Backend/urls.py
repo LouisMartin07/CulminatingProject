1daf5d88a821de6eae69cfe224e8484e37768d7e
LouisMartin07 <louis.martin11730@gmail.com>
2024-04-05 11:42:49 -0500

    making sure secrets arent pushed

---
+++ Backend/Backend/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')), #used for oauth defaults, can remove once React is setup
+    path('', include('accounts.urls'))
+]
