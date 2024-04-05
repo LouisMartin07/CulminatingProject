ff73eeda4797ae59855698267ab3a142ccef45e3
LouisMartin07 <louis.martin11730@gmail.com>
2024-04-05 11:04:43 -0500

    verifying files are hidden

---
+++ Backend/user_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
+def home(request):
+    if request.user.is_authenticated:
+        print(request.user)
+    return render(request, 'home.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
