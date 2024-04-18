dded7d103d679b287d3c72b1553038ad9f5b7869
LouisMartin07 <louis.martin11730@gmail.com>
2024-04-18 09:17:59 -0500

    updating the homepage text

--- Frontend/src/pages/homepage/homepage.jsx
+++ Frontend/src/pages/homepage/homepage.jsx
@@ -11,7 +11,6 @@ const HomePage = () => {
     width: '100%',
   };
 
-  // adding an overlay(image more translucent) to make text more visible 
   const overlayStyle = {
     backgroundColor: 'rgba(0, 0, 0, 0.45)', 
     color: '#fff',
@@ -19,12 +18,27 @@ const HomePage = () => {
     height: '100%', 
   };
 
-  // Style to make the surrounding whitespace black
   const containerStyle = {
     backgroundColor: '#000', 
     padding: 0, 
     margin: 0 
   };
+
+  // Define the text for each image
+  const descriptions = [
+    {
+      title: 'Take Control of Your Apiary',
+      text: 'There are many factors you can’t control, but managing your data shouldn’t be one of them. Our platform offers unmatched data management tools, making it superior to other apiary apps. Here, you control what matters.'
+    },
+    {
+      title: 'Features for All Skill Levels',
+      text: 'Whether you’re a novice or an expert, we have something for everyone. From basic resources like calendars and data tables to advanced analytics and graphs on environmental impacts, our tools help you manage your apiary effectively.'
+    },
+    {
+      title: 'Future of Hive Management',
+      text: 'Imagine more advanced analytics and the ability to upload pictures of your slides. Our future AI will analyze these images and provide a detailed health report, removing the guesswork from hive maintenance.'
+    }
+  ];
 
   return (
     <>
@@ -35,9 +49,9 @@ const HomePage = () => {
               <Card className="text-white text-center">
                 <Card.Img src={image} alt={`Feature ${index + 1}`} style={imageStyle} />
                 <Card.ImgOverlay style={overlayStyle} className="d-flex flex-column justify-content-center">
-                  <Card.Title>{`Feature ${index + 1}`}</Card.Title>
+                  <Card.Title>{descriptions[index].title}</Card.Title>
                   <Card.Text>
-                    {`This section is about Feature ${index + 1}. Add your descriptive text here to explain the feature.`}
+                    {descriptions[index].text}
                   </Card.Text>
                 </Card.ImgOverlay>
               </Card>
